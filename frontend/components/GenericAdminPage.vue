fd113f664be01aa6cb7ce575e85c1a5f593e5065
Quatters <vladnfs3@gmail.com>
2022-04-16 01:07:42 +1000

    Implemented generic admin page

---
+++ frontend/components/GenericAdminPage.vue
@@ -0,0 +1,157 @@
+<template>
+  <b-container fluid>
+    <b-row class="m-4">
+      <b-col cols="7" class="table-wrapper">
+        <my-table
+          :items="items"
+          :fields="fields"
+          @rowSelect="handleRowSelect"
+          ref="table"
+        />
+      </b-col>
+      <b-col>
+        <create-modify-form
+          :form="{ inputs: fields, data: inputData }"
+          :canModify="!!selectedRow"
+          @create="handleCreate"
+          @modify="handleModify"
+        />
+        <small
+          :class="
+            operationStatus.status === 'error' ? 'text-danger' : 'text-muted'
+          "
+          >{{ operationStatus.message }}</small
+        >
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import CreateModifyForm from './CreateModifyForm.vue';
+import MyTable from './MyTable.vue';
+export default {
+  components: { MyTable, CreateModifyForm },
+  props: {
+    fields: {
+      type: Array,
+      required: true,
+    },
+    requestScheme: {
+      type: Array,
+      required: true,
+    },
+    inputScheme: {
+      type: Array,
+      required: true,
+    },
+    endpoint: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      operationStatus: {
+        status: null,
+        message: '',
+      },
+      selectedRow: null,
+      items: [],
+      inputData: {},
+    };
+  },
+  async mounted() {
+    this.items = await this.$axios.$get(this.endpoint);
+    this.fields.forEach(async (field) => {
+      if (field.type === 'select') {
+        const data = await this.$axios.$get(field.optionsFetchEndpoint);
+        field.options = data.map((item) => {
+          return {
+            value: item[field.optionsScheme.value],
+            text: item[field.optionsScheme.text],
+          };
+        });
+      }
+    });
+  },
+  methods: {
+    handleRowSelect(row) {
+      if (!row) return;
+
+      this.selectedRow = { ...row };
+
+      let inputData = {};
+      this.inputScheme.forEach((item) => {
+        inputData[item] = row[item];
+      });
+      this.inputData = inputData;
+    },
+    async handleCreate(data) {
+      try {
+        const sendData = {};
+        this.requestScheme.forEach((key) => {
+          sendData[key] = data[key];
+        });
+        await this.$axios.$post(this.endpoint, sendData);
+        this.items = await this.$axios.$get(this.endpoint);
+        this.operationStatus = {
+          status: 'success',
+          message: `Запись успешно добавлена.`,
+        };
+      } catch (error) {
+        this.operationStatus.status = 'error';
+        if (error.response?.status === 400) {
+          if (error.response.data?.code === 'ER_DUP_ENTRY') {
+            this.operationStatus.message =
+              'Ошибка. Запись с указанным ключом уже существует.';
+          }
+        } else {
+          this.operationStatus.message = 'Неизвестная ошибка.';
+        }
+      } finally {
+        this.inputData = {};
+        this.$refs.table.unselect();
+        this.selectedRow = null;
+      }
+    },
+    async handleModify(data) {
+      let oldData = {};
+      this.requestScheme.forEach((item) => {
+        oldData[item] = this.selectedRow[item];
+      });
+
+      let newData = {};
+      this.requestScheme.forEach((item) => {
+        newData[item] = data[item];
+      });
+
+      try {
+        await this.$axios.$put(this.endpoint, {
+          oldData,
+          newData,
+        });
+        this.items = await this.$axios.$get(this.endpoint);
+        this.operationStatus = {
+          status: 'success',
+          message: 'Запись успешно изменена.',
+        };
+      } catch (error) {
+        this.operationStatus.status = 'error';
+        if (error.response?.status === 400) {
+          if (error.response.data?.code === 'ER_DUP_ENTRY') {
+            this.operationStatus.message =
+              'Ошибка. Запись с указанным ключом уже существует.';
+          }
+        } else {
+          this.operationStatus.message = 'Неизвестная ошибка.';
+        }
+      } finally {
+        this.inputData = {};
+        this.$refs.table.unselect();
+        this.selectedRow = null;
+      }
+    },
+  },
+};
+</script>
--- frontend/pages/admin/cities.vue
+++ frontend/pages/admin/cities.vue
@@ -1,44 +1,18 @@
 <template>
-  <b-container fluid>
-    <b-row class="m-4">
-      <b-col cols="7" class="table-wrapper">
-        <my-table
-          :items="cities"
-          :fields="fields"
-          @rowSelect="handleRowSelect"
-          ref="table"
-        />
-      </b-col>
-      <b-col>
-        <create-modify-form
-          :form="{ inputs: fields, data: inputData }"
-          :canModify="!!selectedRow"
-          @create="handleCreate"
-          @modify="handleModify"
-        />
-        <small
-          :class="
-            operationStatus.status === 'error' ? 'text-danger' : 'text-muted'
-          "
-          >{{ operationStatus.message }}</small
-        >
-      </b-col>
-    </b-row>
-  </b-container>
+  <generic-admin-page
+    :fields="fields"
+    :requestScheme="requestScheme"
+    :inputScheme="inputScheme"
+    endpoint="/api/admin/cities"
+  />
 </template>
 
 <script>
-import CreateModifyForm from '../../components/CreateModifyForm.vue';
-import MyTable from '../../components/MyTable.vue';
+import GenericAdminPage from '../../components/GenericAdminPage.vue';
 export default {
-  components: { MyTable, CreateModifyForm },
+  components: { GenericAdminPage },
   data() {
     return {
-      operationStatus: {
-        status: null,
-        message: '',
-      },
-      selectedRow: null,
       fields: [
         {
           key: 'city',
@@ -47,83 +21,11 @@ export default {
           type: 'text',
         },
       ],
-      cities: [],
-      inputData: {
-        city_id: null,
-        city: null,
-      },
+      requestScheme: ['city'],
+      inputScheme: ['city', 'city_id'],
     };
-  },
-  async mounted() {
-    this.cities = await this.$axios.$get('/api/admin/cities');
-  },
-  methods: {
-    handleRowSelect(row) {
-      if (!row) return;
-
-      this.selectedRow = { ...row };
-      this.inputData = { ...row };
-    },
-    async handleCreate(data) {
-      try {
-        const response = await this.$axios.$post('/api/admin/cities', {
-          city: data.city,
-        });
-        this.cities = await this.$axios.$get('/api/admin/cities');
-        this.operationStatus = {
-          status: 'success',
-          message: `Населенный пункт "${response.city}" успешно добавлен.`,
-        };
-      } catch (error) {
-        this.operationStatus.status = 'error';
-        if (error.response?.status === 400) {
-          if (error.response.data?.code === 'ER_DUP_ENTRY') {
-            this.operationStatus.message =
-              `Ошибка при добавлении записи. ` +
-              `Справочник уже содержит населенный пункт "${data.city}".`;
-          }
-        } else {
-          this.operationStatus.message = 'Неизвестная ошибка.';
-        }
-      } finally {
-        this.inputData = {};
-        this.$refs.table.unselect();
-        this.selectedRow = null;
-      }
-    },
-    async handleModify(data) {
-      const oldData = { ...this.selectedRow };
-      const newData = { ...data };
-
-      console.log(oldData, newData);
-
-      try {
-        const response = await this.$axios.$put('/api/admin/cities', {
-          oldData,
-          newData,
-        });
-        this.cities = await this.$axios.$get('/api/admin/cities');
-        this.operationStatus = {
-          status: 'success',
-          message: `Населенный пункт "${oldData.city}" успешно изменен на "${response.city}".`,
-        };
-      } catch (error) {
-        this.operationStatus.status = 'error';
-        if (error.response?.status === 400) {
-          if (error.response.data?.code === 'ER_DUP_ENTRY') {
-            this.operationStatus.message =
-              `Ошибка при добавлении записи. ` +
-              `Справочник уже содержит населенный пункт "${newData.city}".`;
-          }
-        } else {
-          this.operationStatus.message = 'Неизвестная ошибка.';
-        }
-      } finally {
-        this.inputData = {};
-        this.$refs.table.unselect();
-        this.selectedRow = null;
-      }
-    },
   },
 };
 </script>
+
+<style></style>
